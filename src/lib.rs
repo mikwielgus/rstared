eaf5685b4abb26ee731d53facdd3ac100aca453c
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2025-12-19 03:51:43 +0100

    Make repository REUSE-compliant

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 rstared contributors
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
+
 #![doc(html_root_url = "https://docs.rs/rstared")]
 #![doc = include_str!("../README.md")]
 //#![deny(missing_docs)]
